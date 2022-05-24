3d2e34a3bf63b038e293dbfbf11945b94ebd7645
Clayton2712 <clayton27121996@gmail.com>
2022-05-24 10:54:19 +0200

    created tutoriall controller

---
+++ app/controllers/tutorialController.js
@@ -0,0 +1,19 @@
+const Tutorial = require("../models/tutorialModel.js");
+const {} = require("express");
+
+//Create and save a new Tutorial
+exports.create = (req, res) => {
+
+}
+
+//Retrieve all Tutorials from the database (with condition).
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    Tutorial.getAll(title, (err, data) => {
+        if (err) 
+            res.status(500).send({
+                message: err.message || "Some error occured while retrieving tutorials."
+            });
+        else res.send(data);
+    });
+};
