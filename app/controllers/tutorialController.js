abb70255eb0371b9f6384198c757dee0f4251b68
Clayton2712 <clayton27121996@gmail.com>
2022-05-25 09:48:49 +0200

    created create method in controller

--- app/controllers/tutorialController.js
+++ app/controllers/tutorialController.js
@@ -3,8 +3,27 @@ const {} = require("express");
 
 //Create and save a new Tutorial
 exports.create = (req, res) => {
-
-}
+    //Validation
+    if(!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+    //Create Tutorial
+    const tutorial = new Tutorial({
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published || false
+    });
+    //Save tutorial in database
+    Tutorial.create(tutorial, (err, date) => {
+        if(err)
+        res.status(500).send({
+            message: err.message || "Some error occured while creating the Tutorial."
+        });
+        else res.send(data);
+    });
+};
 
 //Retrieve all Tutorials from the database (with condition).
 exports.findAll = (req, res) => {
