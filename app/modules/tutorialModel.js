81a1dda80dd2ecde35da58b6a29762118c21f80e
Clayton2712 <clayton27121996@gmail.com>
2022-05-26 10:27:35 +0200

    Added findById to model

--- app/modules/tutorialModel.js
+++ app/modules/tutorialModel.js
@@ -58,4 +58,21 @@ Tutorial.updateById = (id, tutorial, result) => {
     );
 };
 
+Tutorial.findById = (id, result) => {
+    sql.query(`SELECT * FROM tutorials WHERE id = ${id}`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+        if (res.length) {
+            console.log("found tutorial: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+        //not found tutorial with id
+        result({kind: "not_found"}, null);
+    });
+};
+
 module.exports = Tutorial;
