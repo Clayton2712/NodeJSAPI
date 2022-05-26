a711367217dd318cac9ce2cdae19f149f7ba0961
Clayton2712 <clayton27121996@gmail.com>
2022-05-26 10:21:00 +0200

    Added findOne method to controller

--- app/controllers/tutorialController.js
+++ app/controllers/tutorialController.js
@@ -37,11 +37,28 @@ exports.findAll = (req, res) => {
     });
 };
 
+//Find single Tutorial with an ID
+exports.findOne = (req, res) => {
+    Tutorial.findById(req.params.id, (arr, data) => {
+        if (err) {
+            if (err.kind === "not_found"){
+                res.status(404).send({
+                    message: `Not found Tutorial with ID: ${req.params.id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: `Error retrieving Tutorial with ID: ${req.params.id}.`
+                });
+            }
+        } else res.send(data);
+    });
+};
+
 exports.update = (req, res) => {
     //Validation request
     if (!req.body) {
         res.status(400).send({
-            message: "COntent can not be empty!"
+            message: "Content can not be empty!"
         });
     }
     console.log(req.body);
@@ -50,7 +67,7 @@ exports.update = (req, res) => {
         new Tutorial(req.body),
         (err, data) => {
             if(err) {
-                if (err.kind === "not found") {
+                if (err.kind === "not_found") {
                     res.status(404).send({
                         message: `Not found Tutorial with ID ${req.params.id}.`
                     });
